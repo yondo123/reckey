4c112d644ee8c14c95c726acc1ed647d17d95ef6
tlatmsrud <tlatmsrud@naver.com>
2021-07-11 00:25:28 +0900

    기본 셋팅

---
+++ reckey/src/main/java/com/reckey/ReckeyApplication.java
@@ -0,0 +1,13 @@
+package com.reckey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReckeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReckeyApplication.class, args);
+	}
+
+}
---
+++ reckey/src/test/java/com/reckey/ReckeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reckey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReckeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
