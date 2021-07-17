a57996b91eb06be6e4088984b2c1a0e4c1726a41
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2021-07-17 23:54:53 +0900

    init : reckey app 기본 구조 커밋

---
+++ reckey/front/reckey-app/src/components/ReckeyHeader.vue
@@ -0,0 +1,13 @@
+<template>
+      <header>
+        <h1 class="title">RECKEY.net</h1>
+        <button type="button"><i class="fas fa-bars"></i></button>
+        <a href="#" class="main-link">RECKEY.net</a>
+        <button type="button"><i class="fas fa-user"></i></button>
+    </header>
+</template>
+<script>
+export default {}
+</script>
+<style>
+</style>
--- reckey/front/reckey-app/src/main.js
+++ reckey/front/reckey-app/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faUser} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './assets/style.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
-createApp(App).mount('#app')
+library.add(faBars, faUser);
+const reckeyApp = createApp(App);
+reckeyApp.component('font-awesone-icon', FontAwesomeIcon);
+reckeyApp.mount('#app');
